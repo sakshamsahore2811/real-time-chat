759f2439d7a867a9d77f6eadc75f86c0fbf724aa
Saksham Sharma <201382@juitsolan.in>
2024-04-02 14:36:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FeaturesComponent } from './Components/features/features.component';
+import { AboutusComponent } from './Components/aboutus/aboutus.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,HeaderComponent,
+    FeaturesComponent,AboutusComponent,FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
